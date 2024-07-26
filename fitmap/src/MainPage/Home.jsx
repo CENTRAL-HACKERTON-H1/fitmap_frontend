b44f33e89a1825af1942ad83834f7cdeb85ad350
yujinko <rhdbwls3355@naver.com>
2024-07-27 07:18:19 +0900

    search 기능 추가

--- fitmap/src/MainPage/Home.jsx
+++ fitmap/src/MainPage/Home.jsx
@@ -1,13 +1,28 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import Nav from './Nav';
+import { useState } from 'react';
+import Search from './Search';
+import styled from 'styled-components';
+import { SloganContainer } from './Home.styled';
 
 const Home = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
     <>
-      <div> main page 내용</div>
+      <SloganContainer>주변의 액티비티를 찾아보세요!</SloganContainer>
+      {/* 서치 */}
+      <div style={{ height: '800px' }}>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
     </>
   );
 };
 
 export default Home;
+
+const SearchBox = styled.input`
+  width: 250px;
+  height: 25px;
+  border: 1px solid lightgrey;
+`;
---
+++ fitmap/src/MainPage/Search.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from 'styled-components';
+
+const Search = ({ searchValue, setSearchValue }) => {
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <SearchBody>
+      <SearchInput
+        placeholder="검색어를 입력하세요"
+        value={searchValue}
+        onChange={handleChange}
+      ></SearchInput>
+    </SearchBody>
+  );
+};
+
+export default Search;
+
+const SearchBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 0;
+`;
+
+const SearchInput = styled.input`
+  width: 250px;
+  height: 25px;
+  border: 1px solid lightgrey;
+`;
